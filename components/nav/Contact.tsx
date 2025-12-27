c616c227547782359844c7be138ed64ad628df9b
Qianqian Wei <qianqianwei112233@gmail.com>
2025-12-27 20:43:59 +0800

    Mobile do not need github

--- components/nav/Contact.tsx
+++ components/nav/Contact.tsx
@@ -18,17 +18,19 @@ const Contact = () => {
       </Link>
 
       {/* GitHub */}
-      <Link
-        href="https://github.com/Qianqian122333"
-        target="_blank"
-        className="relative px-6 py-3 rounded-full font-medium text-white shadow-lg hover:scale-105 active:scale-95 transition-all duration-300 flex items-center gap-2 overflow-hidden"
-      >
-        <div className="absolute inset-0 z-0 bg-linear-to-r from-orange-400 via-orange-550 to-orange-700 transition-transform duration-500 group-hover:scale-110" />
-        <div className="relative z-10 flex items-center gap-2">
-          <Github size={20} />
-          <span>GitHub</span>
-        </div>
-      </Link>
+      <div className="hidden md:block">
+        <Link
+          href="https://github.com/Qianqian122333"
+          target="_blank"
+          className="relative px-6 py-3 rounded-full font-medium text-white shadow-lg hover:scale-105 active:scale-95 transition-all duration-300 flex items-center gap-2 overflow-hidden"
+        >
+          <div className="absolute inset-0 z-0 bg-linear-to-r from-orange-400 via-orange-550 to-orange-700 transition-transform duration-500 group-hover:scale-110" />
+          <div className="relative z-10 flex items-center gap-2">
+            <Github size={20} />
+            <span>GitHub</span>
+          </div>
+        </Link>
+      </div>
 
       {/* Resume Download */}
       <a
